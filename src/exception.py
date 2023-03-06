69abb45e85a1b66473f210c90c583295fb7948aa
Krish Naik <krishnaik06@gmail.com>
2023-03-07 01:02:15 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
+
+
+        
