872ede9011314fc8447206f87b84939edc2d5116
Krish C Naik <krishnaik06@gmail.com>
2023-03-29 01:04:02 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -12,8 +12,10 @@ class PredictPipeline:
         try:
             model_path=os.path.join("artifacts","model.pkl")
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
