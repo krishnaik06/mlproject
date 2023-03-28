a081d94f738ca14a6ce686661c82ad7cdb679d1f
Krish C Naik <krishnaik06@gmail.com>
2023-03-29 01:10:23 +0530

    Update application.py

--- application.py
+++ application.py
@@ -32,9 +32,12 @@ def predict_datapoint():
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
+        print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
         return render_template('home.html',results=results[0])
     
 
