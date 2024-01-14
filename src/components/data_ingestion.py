28ee29620976b7003e13043816e415b57708bc7b
12ankitsingh99 <52731346+12ankitsingh99@users.noreply.github.com>
2024-01-14 16:20:22 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,7 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
